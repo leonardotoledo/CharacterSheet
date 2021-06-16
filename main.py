6bcd79fb0037755514d7731fa04513e93c15c197
Leonardo Tolêdo <contato@leonardotoledo.org>
2021-06-16 18:23:18 -0300

    added .env

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from keep_alive import keep_alive
 import discord
 import os
 import json
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 client = discord.Client()
 
@@ -13,7 +16,7 @@ async def write_json(dictionary):
   json_object = json.dumps(dictionary, indent = 4)
     
   # Writing to sample.json
-  with open(dictionary["user"]+".json", "w") as outfile:
+  with open(dictionary['user']+'.json', 'w') as outfile:
     outfile.write(json_object)
 
 @client.event
@@ -26,9 +29,10 @@ async def on_message(message):
     return
 
   if message.content.startswith('!create-sheet'):
-    dictionary["user"] = message.author
+    dictionary['user'] = message.author.name
+    print(dictionary)
     await write_json(dictionary)
     await message.channel.send('New sheet created!')
 
 keep_alive()
-client.run(os.environ['TOKEN'])
+client.run(os.getenv('TOKEN'))
