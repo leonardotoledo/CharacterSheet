aff7cd51776585529f2a61e345a8a669fb23595f
Leonardo Tolêdo <leonardotoledo@outlook.com>
2021-06-16 19:13:34 +0000

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from keep_alive import keep_alive
 import discord
 import os
+import json
 
 client = discord.Client()
 
+# Data to be written
+dictionary = {}
+
+async def write_json(dictionary):
+  # Serializing json 
+  json_object = json.dumps(dictionary, indent = 4)
+    
+  # Writing to sample.json
+  with open(dictionary["user"]+".json", "w") as outfile:
+    outfile.write(json_object)
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+  if message.content.startswith('!create-sheet'):
+    dictionary["user"] = message.author
+    await write_json(dictionary)
+    await message.channel.send('New sheet created!')
 
 keep_alive()
 client.run(os.environ['TOKEN'])
